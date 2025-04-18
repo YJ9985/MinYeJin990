9dd5454a869ac089ce12c3e0544d5ca7ba2f1ef6
Yeji <lucy1486@naver.com>
2025-04-18 22:04:37 +0900

    f/follow

--- accounts/views.py
+++ accounts/views.py
@@ -105,7 +105,7 @@ def follow(request, user_pk):
     User = get_user_model()
     person = User.objects.get(pk=user_pk)
     if person != request.user:
-        if person in person.followers.all():
+        if request.user in person.followers.all():
             person.followers.remove(request.user)
         else:
             person.followers.add(request.user)
